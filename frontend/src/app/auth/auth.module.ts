06750ff52b6b841085233f0f7a1a5ee5dbf1ec59
Harshad Sonagara <99785847+HarshadSonagara2205@users.noreply.github.com>
2022-11-21 22:13:13 +0530

    add navbar and angular setup

---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '../shared/angular-material/material.module';
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    MaterialModule,
+    CommonModule
+  ]
+})
+export class AppModule { }
